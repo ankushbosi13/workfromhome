cb0e7260fbc9e7240346c64ecfc84f8da0c49709
AnkushBosi <46882503+ankushbosi13@users.noreply.github.com>
2020-03-28 20:28:35 +0530

    Add files via upload

---
+++ DAY_1 DT-27-03-2020/numbers.py
@@ -0,0 +1,25 @@
+print(4+3)
+name = input("enter your name !!")
+print("hi" +  name )
+
+age = input("enter your age")
+
+age=int(age)#type casting 
+
+print(age)
+
+age1=input("enter the age")
+print(age)
+
+print(pow(5,2))
+print(abs(-29))
+print(round(4.38))
+#print(ceil(4.92))
+print(bin(23))
+print(hex(33))
+
+pi=input("enter the value") 
+pi=float(pi)#type casting
+
+print(10/3)
+print(2**3)
